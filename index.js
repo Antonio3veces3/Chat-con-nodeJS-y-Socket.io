bd55c85774c465dfaab4eb43f36d6748872cc068
Antonio Ramirez <cramirez28@ucol.mx>
2021-04-29 00:18:32 -0500

    version con mysql

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 var app= require('express')();
 var http= require('http').Server(app);
 var io= require('socket.io')(http);
+const { connect } = require('http2');
+var mysql= require('mysql'); 
+
+var conn= mysql.createConnection({ //crear conexion con mysql
+    host: 'localhost',
+    database: 'chat',
+    user: 'root',
+    password: ''
+})
+
+conn.connect((err)=>{
+    if(err)
+    throw err;
+    else
+    console.log('Conexion exitosa');
+})
+
 app.get('/',(req,res)=>{ //funcion que redirecciona localhost a chat.html
     res.sendFile(__dirname+'/chat.html');
 });
@@ -11,9 +28,14 @@ io.on('connection',(socket)=>{ //crea la conexion del usuario
     var username;
     socket.on('crearUser',(data)=>{ //crea un usuario
         username= data;
+        conn.query('INSERT INTO usuarios(nombre_usuario,fecha) VALUES ("'+username+'", CURDATE())'); 
     });
 
     socket.on('msjNuevo', (data)=>{ //crea el nuevo mensaje
+        socket.broadcast.emit('mensaje',{
+            usuario: username,
+            mensaje: data 
+        });
         socket.emit('mensaje',{
            usuario: username,
            mensaje: data 
